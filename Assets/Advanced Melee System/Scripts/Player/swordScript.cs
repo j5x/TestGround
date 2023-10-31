21959e31a83d7e0e74855cc012935634b95f656b
j5x <32881@ma-web.nl>
2023-10-31 15:57:44 +0100

    added new particle effects
    new block/parry particle effects

--- Assets/Advanced Melee System/Scripts/Player/swordScript.cs
+++ Assets/Advanced Melee System/Scripts/Player/swordScript.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class swordScript : MonoBehaviour{
     
+    [SerializeField] private float postureDMG;
+    [SerializeField] UnityEvent OnSuccesFullBlock;
     private void OnCollisionEnter(Collision collision)
+
     {
         if (collision.collider.CompareTag("Sword"))
         {
@@ -12,7 +16,8 @@ public class swordScript : MonoBehaviour{
             if (playerCombat.blocking)
             {
                 Debug.Log("Sword is hit");
-                playerCombat.ApplyPostureDamage(25.0f); // Adjust the value as needed
+                playerCombat.ApplyPostureDamage(postureDMG); // Adjust the value as needed
+                OnSuccesFullBlock?.Invoke();
             }
         }
     }
