ef6d0484f087f1e6f5a832c6e187b99801932b85
j5x <32881@ma-web.nl>
2023-11-02 12:03:32 +0100

    Fixed movement mechanics and gravity, Added jump feature for parkour

--- Assets/Advanced Melee System/Scripts/Player/PlayerMove.cs
+++ Assets/Advanced Melee System/Scripts/Player/PlayerMove.cs
@@ -2,63 +2,71 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class PlayerMove : MonoBehaviour
 {
- 
- 
     CharacterController Controller;
- 
     public float Speed;
-
+    public float JumpHeight;
+    private bool isGrounded;
+    private bool isJumping = false;
+    private float verticalSpeed = 0f;
     [SerializeField] private Animator anim;
-    
     public Transform Cam;
- 
- 
+
     // Start is called before the first frame update
     void Start()
     {
- 
         Controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
     }
- 
+
     // Update is called once per frame
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-        float Vertical = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        isGrounded = Controller.isGrounded;
 
-        if (Input.GetKeyDown(KeyCode.W))
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        if (isGrounded && verticalSpeed < 0)
+        {
+            verticalSpeed = 0f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+            verticalSpeed = Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
         {
             anim.SetBool("isWalking", true);
         }
 
-        if (Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
         {
             anim.SetBool("isWalking", false);
         }
- 
-        Vector3 Movement = Cam.transform.right * Horizontal + Cam.transform.forward * Vertical;
+
+        Vector3 Movement = new Vector3(Horizontal, 0, Vertical);
+        Movement = Cam.transform.TransformDirection(Movement);
         Movement.y = 0f;
- 
- 
- 
-        Controller.Move(Movement);
- 
+        Movement.Normalize();
+        Movement *= Speed * Time.deltaTime;
+
         if (Movement.magnitude != 0f)
         {
-            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Cam.GetComponent<CameraMove>().sensivity * Time.deltaTime);
- 
- 
-            Quaternion CamRotation = Cam.rotation;
-            CamRotation.x = 0f;
-            CamRotation.z = 0f;
- 
-            transform.rotation = Quaternion.Lerp(transform.rotation, CamRotation, 0.1f);
- 
+            transform.rotation = Quaternion.LookRotation(new Vector3(Movement.x, 0, Movement.z));
         }
+
+        if (!isGrounded)
+        {
+            verticalSpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        Movement.y = verticalSpeed * Time.deltaTime;
+        Controller.Move(Movement);
     }
- 
 }
